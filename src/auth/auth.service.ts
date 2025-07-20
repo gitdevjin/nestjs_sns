689b5bb220eaaad43ea8c719cc0f3a4abf6f50fb
gitdevjin <dev.hjshin@gmail.com>
2025-07-20 00:07:45 -0400

    auth module added

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,60 @@
+import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { UsersModel } from 'src/users/entities/users.entity';
+import { JWT_SECRET } from './const/auth.const';
+
+@Injectable()
+export class AuthService {
+  constructor(private readonly jwtService: JwtService) {}
+  /**
+   * To do
+   *
+   * 1) registerWithEmail
+   *    - email, nickname, password,
+   *    - return accessToken, refreshToken
+   *
+   * 2) loginWithEmail
+   *    - enter email and password,
+   *    - return accessToken and refreshToken
+   *
+   * 3) loginUser
+   *    - function that returns accessToekn and refreshToken
+   *
+   * 4) signToken
+   *    - generate accessToken and refreshToken
+   *
+   * 5) authenticateWithEmailandPassword
+   *    - authentication logic
+   *      1. check if the user(email) exists
+   *      2. check password
+   *      3. return user info
+   *
+   *
+   */
+
+  /**
+   * payload
+   * 1. email
+   * 2. sub -> id
+   * 3. type : 'access' or 'refresh'
+   */
+  signToken(user: Pick<UsersModel, 'email' | 'id'>, isRefreshToken: boolean) {
+    const payload = {
+      email: user.email,
+      sub: user.id,
+      type: isRefreshToken ? 'refresh' : 'access',
+    };
+
+    return this.jwtService.sign(payload, {
+      secret: JWT_SECRET,
+      expiresIn: isRefreshToken ? 3600 : 300,
+    });
+  }
+
+  loginUser(user: Pick<UsersModel, 'email' | 'id'>) {
+    return {
+      accessToken: this.signToken(user, false),
+      refreshToken: this.signToken(user, true),
+    };
+  }
+}
